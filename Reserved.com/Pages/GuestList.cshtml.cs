2280aec640132bdcbb51f06cc185af023500af17
Taylor <shartayl@LUK-5CG2500W6Q>
2023-06-29 09:48:18 +0100

    Add project files.

---
+++ Reserved.com/Pages/GuestList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reserved.com.Pages
+{
+    public class GuestListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
